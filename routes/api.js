c9f5c4f2dfe798c1c35e684c839f2d2d4b064c0d
Jessica Helft <jessicahelft@gmail.com>
2020-07-12 20:44:30 -0500

    added api routes

---
+++ routes/api.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const db = require("../models");
+
+const router = express.Router();
+
+//All workouts
+router.get("/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  
+  });
+
+  //post new workout
+router.post("/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+//updated workout
+router.put("/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { exercises: req.body },
+      },
+      { useFindAndModify: false }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+});
+
+//workout range
+router.get("/workouts/range", (req, res) => {
+    db.Workout.find({}).sort({ day: -1 }).limit(7)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts)
+    }).catch((err) => {
+        res.json(err);
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(htmlRoutes);
+app.use("/api", apiRoutes);
 
 
 
